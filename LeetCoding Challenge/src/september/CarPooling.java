7eb63db4602b06adff453772283f80ef173cf0d4
Tarun Sharma <tarun10994@gmail.com>
2020-09-22 19:26:05 +0530

    September CarPooling

--- LeetCoding Challenge/src/september/CarPooling.java
+++ LeetCoding Challenge/src/september/CarPooling.java
@@ -1,35 +1,26 @@
 package september;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /*
 https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/556/week-3-september-15th-september-21st/3467/
  */
 public class CarPooling {
     public boolean carPooling(int[][] trips, int capacity) {
-        List<Location> list=new ArrayList<>();
-        for (int i = 0; i < trips.length; i++) {
-            list.add(new Location(trips[i][1],trips[i][0]));
-            list.add(new Location(trips[i][2],-trips[i][0]));
-        }
-        list.sort((o1, o2) -> o1.point!=o2.point?o1.point-o2.point:o1.passengers-o2.passengers);
-        int passengeOnBoard=0;
-        for (Location location:list)
+        int[] timeLine=new int[1001];
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int[] trip:trips)
         {
-            passengeOnBoard += location.passengers;
-            if(passengeOnBoard > capacity)
+            timeLine[trip[1]] += trip[0]; // Boarding Passenger
+            timeLine[trip[2]] -= trip[0]; // UnBoarding Passenger
+            min=Math.min(min,trip[1]);
+            max=Math.max(max,trip[2]);
+        }
+        int total=0;
+        for (int i = min; i <= max; i++) {
+            total += timeLine[i];
+            if(total > capacity)
                 return false;
         }
         return true;
-    }
-    static class Location{
-        int point;
-        int passengers;
-
-        public Location(int point, int passengers) {
-            this.point = point;
-            this.passengers = passengers;
-        }
     }
 }
