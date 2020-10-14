f5be23e6c346b70035b0a102040b2af3d2d5db50
Tarun Sharma <tarun10994@gmail.com>
2020-10-14 20:08:52 +0530

    September WordBreak

---
+++ LeetCoding Challenge/src/september/WordBreak.java
@@ -0,0 +1,26 @@
+package september;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/558/week-5-september-29th-september-30th/3477/
+ */
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        dp[0] = true;
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(wordDictSet.contains(s.substring(j, i)) && dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
