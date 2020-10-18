e03ab5e242af782bd2746340a783d72851457ec6
Tarun Sharma <tarun10994@gmail.com>
2020-10-18 19:05:24 +0530

    October RepeatedDNASequences

--- LeetCoding Challenge/src/october/RepeatedDNASequences.java
+++ LeetCoding Challenge/src/october/RepeatedDNASequences.java
@@ -1,37 +1,45 @@
 package october;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class RepeatedDNASequences {
-    public List<String> findRepeatedDnaSequences(String s) {
-        int counter=0;
-        Set<String> hasDNA=new HashSet<>();
-        Set<String> processedDNA=new HashSet<>();
-        List<String> answer=new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch=s.charAt(i);
-            if(ch=='A' ||ch=='G' ||ch=='C' ||ch=='T' )
-                counter++;
-            else
-                counter=0;
-
-            if(counter >= 10)
-            {
-                String temp = s.substring(i - 9, i + 1);
-                if (hasDNA.contains(temp)) {
-                        answer.add(temp);
-                        processedDNA.add(temp);
-                        hasDNA.remove(temp);
-
-                }else if(!processedDNA.contains(temp))
-                {
-                    hasDNA.add(temp);
-                }
-            }
+    private static int hashCodeHelper(final Character c) {
+        switch (c) {
+            case 'A':
+                return 0;
+            case 'C':
+                return 1;
+            case 'G':
+                return 2;
+            case 'T':
+                return 3;
+            default:
+                return -1;
         }
-        return answer;
+    }
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> ans = new ArrayList<>(44);
+        if (s.length() < 10) return ans;
+        char[] chars = s.toCharArray();
+        boolean[] hashCodeMap = new boolean[1048576];
+        boolean[] added = new boolean[1048576];
+
+        int hashCode = 0;
+        int i = 0;
+        for (; i < 10; i++) {
+            hashCode = (hashCode << 2) + hashCodeHelper(chars[i]);
+        }
+        hashCodeMap[hashCode] = true;
+        for (; i < chars.length; i++) {
+            hashCode = ((hashCode << 2) & 1048575) + hashCodeHelper(chars[i]);
+            if (hashCodeMap[hashCode] && !added[hashCode]) {
+                ans.add(new String(chars,i-9,10));
+                added[hashCode] = true;
+            }
+            hashCodeMap[hashCode] = true;
+        }
+
+        return ans;
     }
 }
