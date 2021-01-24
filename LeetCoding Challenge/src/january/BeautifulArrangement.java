56acc8e12fe76c4463f4220983369986fa8592d6
Tarun Sharma <tarun10994@gmail.com>
2021-01-24 20:20:02 +0530

    January BeautifulArrangement

---
+++ LeetCoding Challenge/src/january/BeautifulArrangement.java
@@ -0,0 +1,25 @@
+package january;
+/*
+https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3591/
+ */
+public class BeautifulArrangement {
+    public int countArrangement(int n) {
+        int mask=0;
+        return count(mask, 1,n);
+    }
+    int count(int mask,int index,int n)
+    {
+        if(index==n+1){
+            return 1;
+        }
+        int co=0;
+        for (int i = 1; i <= n; i++) {
+            if(!((mask & 1<<i)>0) && (index%i==0 || i%index==0 )){
+                mask = mask | (1<<i);
+                co+=count(mask,index+1,n);
+                mask = mask & ~(1<<i);
+            }
+        }
+        return co;
+    }
+}
