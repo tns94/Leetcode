31705362ea2d9d7c2f570cb9d4836f35485f534b
Tarun Sharma <tarun10994@gmail.com>
2021-02-01 20:16:34 +0530

    January CreateSortedArraythroughInstructions

---
+++ LeetCoding Challenge/src/january/CreateSortedArraythroughInstructions.java
@@ -0,0 +1,44 @@
+package january;
+/*
+https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3599/
+ */
+public class CreateSortedArraythroughInstructions {
+    static void update(int[] tree,int index,int num,int n)
+    {
+        while(index<=n)
+        {
+            tree[index]+=num;
+            index+=index&(-index);
+        }
+    }
+    static int query(int[] tree,int index,int n)
+    {
+        int sum=0;
+        while(index>0)
+        {
+            sum+=tree[index];
+            index-=index&(-index);
+        }
+        return sum;
+    }
+    public int createSortedArray(int[] instructions) {
+        int sum=0;
+        int n=100001;
+        int[] fenwickTree=new int[n];
+        for (int i = 0; i < instructions.length; i++) {
+            int queryOneLess = query(fenwickTree,instructions[i]-1,n);
+            int queryCurr = query(fenwickTree,instructions[i],n);
+            int min = Math.min(queryOneLess,i-queryCurr);
+            System.out.println("Insert "+instructions[i]+" with cost min("+queryOneLess+","+(i-queryCurr)+")-->"+min);
+            sum = (sum + min) % 1000000007 ;
+            update(fenwickTree,instructions[i],1,n);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        CreateSortedArraythroughInstructions c=new CreateSortedArraythroughInstructions();
+        int[] ins={1,2,3,6,5,4};
+        System.out.println(c.createSortedArray(ins));
+    }
+}
